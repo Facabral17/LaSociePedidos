46f74d85e990d2299c9885357863bf82c09dd3cf
Facundo Cabral <Facabral17@gmail.com>
2025-02-13 11:21:21 -0300

    Got rid of login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/Home"; 
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 import "./App.css";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+    // <Router>
+    //   <Routes>
+    //     {/* <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
         
-        <Route
-          path="/"
-          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </Router>
+    //     <Route
+    //       path="/"
+    //       element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+    //     /> */}
+    //   </Routes>
+    // </Router>
+  <Home />
   );
 }
 
